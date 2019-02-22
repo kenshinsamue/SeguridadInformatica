95914b42a74b7a3c6f3a3601b694ed196d6b28d1
k1k4ss0 <predatoren1@gmail.com>
2019-02-22 15:00:25 +0000

    vigenere

--- Vigenere/Vigenere/Program.cs
+++ Vigenere/Vigenere/Program.cs
@@ -8,8 +8,93 @@ namespace Vigenere
 {
     class Program
     {
+
+         public class Vingenere{
+ 
+            string mensaje, clave,cifrado,descifrado;
+            public  Vingenere(string a,string b) {
+
+                mensaje = a;
+                clave = b;
+
+            }
+            public void show_me_da_cyf() {
+
+           
+                Console.WriteLine(cifrado);
+            }
+            public void cyf() {
+
+                int a = 0;
+                int b = 0;
+                int result = 0;
+                int index = 0;
+                for(int i = 0; i < mensaje.Length; i++)
+                {
+                    a = (int)mensaje[i];
+                    b = (int)clave[index];
+                    Console.WriteLine(mensaje[i] + ":" + a);
+                    Console.WriteLine(clave[index] + ":" + b);
+                    result = (a + b)%25+65;
+                    Console.WriteLine((char)result + ":"+result+", resultado divicion:"+);
+                    cifrado = cifrado + (char)result;
+
+
+                    index++;
+                    if(index>= clave.Length)
+                    {
+                        index = 0;
+                    }
+
+                }
+
+            }
+
+            public int get_div() {
+
+
+
+            }
+
+            public void descyf() {
+
+                int a = 0;
+                int b = 0;
+                int result = 0;
+                int index = 0;
+                for(int i=0; i < cifrado.Length; i++)
+                {
+                    a = (int)cifrado[i];
+                    b = (int)clave[index];
+
+
+
+
+                }
+
+
+            }
+        }
+
         static void Main(string[] args)
         {
+
+
+            string mensaje,clave;
+
+
+            Console.WriteLine("introduzca el mensaje a encriptar: ");           // pedimos el mensaje a encriptar y lo guardamos
+            mensaje=Console.ReadLine();
+            mensaje=mensaje.ToUpper();
+            Console.WriteLine("Introduzca la clave con la que encriptar: ");        // pedimos la clave con la que encriptar y la guardamos
+            clave = Console.ReadLine();
+            clave=clave.ToUpper();
+            Vingenere cifrar = new Vingenere(mensaje, clave);                           // creamos el objeto que nos permite cifrar y descifrar
+            cifrar.cyf();
+            cifrar.show_me_da_cyf();
+            Console.ReadLine();    
+
+
         }
     }
 }
