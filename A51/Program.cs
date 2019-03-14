c018de5e2384d80e5ef2c98819f44869335406fe
k1k4ss0 <predatoren1@gmail.com>
2019-03-14 15:28:27 +0000

    asdf

--- A51/Program.cs
+++ A51/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
-
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace A51
 {
     class Program
     {
+
+
+        class LFSR{
+
+            BitArray lfsr;
+            LFSR(int a){
+
+                lfsr= new BitArray(a);
+
+            }
+            public void set_array(int a){
+
+                if(a==19){
+
+                }
+                if(a==22){}
+                if(a==23){}
+
+                else{
+
+                    throw new ArgumentException ("Error, al definir LFSR, la longitud "+a+" no es valida");
+                }
+
+
+            }
+
+        };
+
+        class A5{
+
+
+
+        };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            dafsdfasdf
+            
         }
     }
 }
